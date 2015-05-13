8d41b41f974e2627e9972d4809da22308e12e261
mwmartin <mwm.cal@gmail.com>
2015-05-13 08:40:29 -0600

    fixing analyze-race.sh

--- hadoop/share/analyze-race.sh
+++ hadoop/share/analyze-race.sh
@@ -10,6 +10,6 @@ bin/hdfs dfs -copyFromLocal /share/data/race/* /hdfs/race/
 
 bin/hdfs fs -rm -r /hdfs-output/race
 
-bin/hadoop jar /usr/local/hadoop-2.6.0/share/hadoop/tools/lib/hadoop-streaming-2.6.0.jar -input "/hdfs/race" -output "/hdfs-output/race" -mapper "mono /share/Debug/Mapper.exe"
+bin/hadoop jar /usr/local/hadoop-2.6.0/share/hadoop/tools/lib/hadoop-streaming-2.6.0.jar -input "/hdfs/race" -output "/hdfs-output/race" -mapper "mono /share/race/Debug/Mapper.exe"
 
 bin/hdfs dfs -cat /hdfs-output/race/*
